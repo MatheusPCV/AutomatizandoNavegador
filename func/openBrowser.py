a161024ec3ceaeee5313d65170a46c8b0ddddfa0
MatheusPCV <matheus.p.vieiraa@gmail.com>
2023-11-22 17:40:23 -0300

    feat: adicionando a função para maximizar tela

--- func/openBrowser.py
+++ func/openBrowser.py
@@ -1,9 +1,8 @@
-import os
-import pandas as pd
 from playwright.sync_api import sync_playwright, Playwright
+import os
 
 
-def main(p):
+def openBrowser(p):
     app_data_path = os.getenv("LOCALAPPDATA")
     user_data_path = os.path.join(app_data_path, "Chromium\\User Data\\Default")
     browser = p.chromium.launch_persistent_context(
@@ -13,14 +12,5 @@ def main(p):
         args=["--start-maximized"],
         no_viewport=True,
     )
-    page = browser.new_page()
-    page.goto("https://pt.pons.com/tradu%C3%A7%C3%A3o")
-    page.wait_for_timeout(5000)
-    browser.close()
-    arq = pd.read_excel("./consulta/consulta.xlsx", header=None)
-    valor = arq.iloc[0, 0]
-    print(valor)
 
-
-with sync_playwright() as p:
-    main(p)
+    return browser
